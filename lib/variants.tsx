dc3a138ebf6c522d7b544f36f9f1f05672e778c0
codeartisanlab <ishaan@codeartisanlab.com>
2026-01-18 10:58:52 +0530

    done with the home page

--- lib/variants.tsx
+++ lib/variants.tsx
@@ -1,19 +1,22 @@
-export const fadeIn = (direction: string, delay = 0) => {
-  return {
-    hidden: {
-      opacity: 0,
-      y: direction === "up" ? 50 : direction === "down" ? -50 : 0,
-      x: direction === "left" ? 50 : direction === "right" ? -50 : 0,
+import { Variants } from "framer-motion";
+
+export const fadeIn = (
+  direction: "up" | "down" | "left" | "right",
+  delay: number,
+): Variants => ({
+  hidden: {
+    opacity: 0,
+    y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
+    x: direction === "left" ? 40 : direction === "right" ? -40 : 0,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    x: 0,
+    transition: {
+      delay,
+      duration: 0.6,
+      ease: "easeOut",
     },
-    show: {
-      opacity: 1,
-      x: 0,
-      y: 0,
-      transition: {
-        delay,
-        duration: 0.9,
-        ease: "linear",
-      },
-    },
-  };
-};
+  },
+});
